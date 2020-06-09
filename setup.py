76620f0e241564989e2fb25390c563620c16d710
noisycomputation <noisycomputation@gmail.com>
2020-06-09 01:22:47 -0600

    Make setup.py consistent with siblings

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
+from setuptools import setup
 import io
 import json
-from setuptools import setup
 
-with open('package.json') as f:
+with open("package.json") as f:
     package = json.load(f)
 
 package_name = str(package["name"].replace(" ", "_").replace("-", "_"))
 
 setup(
-    name='dash_html_components-noisycomputation',
+    name=package_name,
     version=package["version"],
-    author=package['author'],
-    author_email='chris@plotly.com',
-    packages=[package_name],
-    url='https://github.com/plotly/dash-html-components',
+    author=package["author"],
+    packages=[package_name.replace("_noisycomputation", "")],
+    url='https://github.com/noisycomputation/dash-html-components',
     include_package_data=True,
-    license=package['license'],
-    description=package['description'] if 'description' in package else package_name,
+    license=package["license"],
+    description=package.get("description", package_name),
     long_description=io.open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    install_requires=[]
+    install_requires=[],
 )
